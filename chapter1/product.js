7f77b3b5812d42ca8d46b9674218feaf308e7e96
Sthabi99 <137986434+Sthabi99@users.noreply.github.com>
2023-09-26 11:25:22 +0200

    work npm

---
+++ chapter1/product.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+class Products extends Component {
+render() {
+return (
+<div>
+<h2>
+Products
+Beginning React
+</h2>
+</div>
+);
+}
+}
+export default Products;
