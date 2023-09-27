7d84e860acea127e7fdc9bcac5dc16a7b06494a8
Sthabi99 <137986434+Sthabi99@users.noreply.github.com>
2023-09-27 12:20:30 +0200

    chapter03

--- chapter3/src/App.js
+++ chapter3/src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from "react";
-import { Button } from 'react-bootstrap';
+import { Button } from "react-bootstrap";
 import Products from "./Products";
-
+import Rating from './Rating';
 class App extends Component {
   render() {
     return (
       <div>
         <h1>My First React App!</h1>
-        <Products /><Button>Default</Button>
+        <Rating rating="1" />
+        <Rating rating="2" />
+        <Rating rating="3" />
+        <Rating rating="4" />
+        <Rating rating="5" />
+        <Products />
+        <Button>Default</Button>
       </div>
     );
   }
